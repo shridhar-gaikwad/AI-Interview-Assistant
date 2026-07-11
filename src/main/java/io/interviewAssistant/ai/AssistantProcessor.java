c168d5919d1b51abb5a3e54a8877a001e5b2e9b1
shridhar-gaikwad <shridhargaikwad333@gmail.com>
2026-07-12 02:13:29 +0530

    "POM FILE SETUP AND PROCESSOR CLASS CREATED."

--- src/main/java/io/interviewAssistant/ai/AssistantProcessor.java
+++ src/main/java/io/interviewAssistant/ai/AssistantProcessor.java
@@ -1,4 +1,13 @@
 package io.interviewAssistant.ai;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AssistantProcessor {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistantProcessor.class, args);
+    }
+
 }
